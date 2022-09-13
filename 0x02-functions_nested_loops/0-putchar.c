045a7cdd97cd8c6b47e16bf33990a9ebee9c92d7
Domicery <dominicokoro2003@gmail.com>
2022-09-13 16:51:55 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*main print putchar
+
+*Return: always 0
+*/
+
+int main(void)
+
+{
+_putchar("");
+
+_putchar('p"); 
+
+_putchar("u");
+
+_putchar('t'); 
+
+_putchar('c');
+
+_putchar("h");
+
+_putchar('a');
+
+_putchar('');
+
+_putchar("\n");
+
+return(0):
+}
